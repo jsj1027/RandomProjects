e5646af1ceee64d2e93e6a01171ead7b275ae2ff
jsj1027 <javisjohnson1@gmail.com>
2018-09-26 23:42:53 -0400

    Added Formatting

--- Tip_Calculator/javafiles/Tipper.java
+++ Tip_Calculator/javafiles/Tipper.java
@@ -1,5 +1,4 @@
 package javafiles;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,10 +21,11 @@ class Tipper{
     private ArrayList<Character> backlog = new ArrayList<>();
 
     public Tipper(String mode) {
-        if(mode.toLowerCase() == "cli")
+        if(mode.equalsIgnoreCase( "cli"))
             setCli_or_web(true);
-        else if(mode.toLowerCase() == "web")
+        else if(mode.equalsIgnoreCase("web"))
             setCli_or_web(false);
+
         System.out.println("Tipper Online");
     }
 
@@ -92,56 +92,72 @@ class Tipper{
     public void calculateBillTotal(){
         double bill = getBill_amount();
         float tip_percent = getTip_percentage();
+
         bill = bill + (bill*tip_percent);
-        setBill_amount(bill);
-        return;
+        setTotal(bill);
     }
 
     public void calculateTipDollarAmount(){
         double tip_dollar_amount = getTip_percentage()*getBill_amount();
+
         setTip_dollar_amount(tip_dollar_amount);
-        return;
     }
 
     public void addToBacklog(char addition){
         ArrayList<Character> backlog = getBacklog();
+
         backlog.add(addition);
         setBacklog(backlog);
-        return;
     }
 
     public void removeFromBacklog(char removal){
         ArrayList<Character> backlog = getBacklog();
+
         backlog.remove(removal);
         setBacklog(backlog);
-        return;
     }
 
     private static void startup(Tipper tipper){
         Scanner scanner = new Scanner( System.in );
-        System.out.println("This is a tip calculator");
-        System.out.println("How much was the bill?");
+
+        System.out.println("\nHow much was the bill?");
         tipper.setBill_amount(Double.parseDouble(scanner.nextLine()));
-        System.out.println("How much are you looking to tip? This should be the percentage number. Ex: '20%' enter 20");
+
+        System.out.println("\nHow much are you looking to tip? This should be the percentage number. Ex: '20%' enter 20");
         tipper.setTip_percentage(Integer.parseInt(scanner.nextLine()));
-        System.out.println("Do you wish to round your bill up to the nearest dollar amount? Enter (yes/y/no/n");
+
+        System.out.println("\nDo you wish to round your bill up to the nearest dollar amount? Enter (yes/y/no/n)");
         String round = scanner.nextLine();
+
         if(round.equalsIgnoreCase("y") || round.equalsIgnoreCase("yes")){
             tipper.setRoundedUp(true);
         }
         else if(round.equalsIgnoreCase("n") || round.equalsIgnoreCase("no")){
             tipper.setRoundedUp(false);
         }
+
         tipper.calculateBillTotal();
         tipper.calculateTipDollarAmount();
-        String ouput = String.format("%f, %f, %f, %f", tipper.getTotal(), tipper.getTip_percentage(),
-                tipper.getTip_dollar_amount(), tipper.getRound_amount());
-        System.out.println(ouput);
+
+        String output = String.format(
+        // BuildMyString.com generated code.
+                "\nBill Amount: %.2f\n" +
+                "Tip Percentage: %.2f\n" +
+                "Tip Amount: %.2f\n" +
+                "Rounded Amount: %.2f\n" +
+                "Total: %.2f\n",
+
+                tipper.getBill_amount(), tipper.getTip_percentage(),
+                tipper.getTip_dollar_amount(), tipper.getRound_amount(), tipper.getTotal());
+
+        System.out.println(output);
     }
 
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("We are starting now");
+
         Tipper tip_calculator = new Tipper("cli");
+
         startup(tip_calculator);
     }
 
